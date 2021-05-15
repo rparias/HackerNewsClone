98a7bb8da903e654a46098c12c8279a7bce3b461
Ronald Arias <ariasron@hotmail.com>
2021-05-15 10:44:19 -0500

    Installs prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
- 
+
 module.exports = {
   entry: path.resolve(__dirname, './src/index.js'),
   module: {
@@ -7,12 +7,12 @@ module.exports = {
       {
         test: /\.(js)$/,
         exclude: /node_modules/,
-        use: ['babel-loader']
-      }
-    ]
+        use: ['babel-loader'],
+      },
+    ],
   },
   resolve: {
-    extensions: ['*', '.js']
+    extensions: ['*', '.js'],
   },
   output: {
     path: path.resolve(__dirname, './dist'),
