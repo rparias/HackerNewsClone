c8c417c52bc43e045c27b9b9faf267427b9d3a72
Ronald Arias <ariasron@hotmail.com>
2021-05-16 13:21:10 -0500

    Fetch stories from external endpoint and adds supports for async await with babel

--- src/pages/stories.js
+++ src/pages/stories.js
@@ -1,5 +1,34 @@
 import view from '../utils/view';
 
-export default function Stories(path) {
-  view.innerHTML = `<div>${path}</div>`;
+const BASE_URL = 'https://node-hnapi.herokuapp.com';
+
+async function getStories(path) {
+  let routePath;
+  switch (path) {
+    case '/':
+      routePath = '/news';
+      break;
+    case '/new':
+      routePath = '/newest';
+      break;
+    case '/ask':
+      routePath = '/ask';
+      break;
+    case '/show':
+      routePath = '/show';
+      break;
+    default:
+      routePath = '';
+  }
+  const response = await fetch(`${BASE_URL}${routePath}`);
+  const stories = await response.json();
+  return stories;
+}
+
+export default async function Stories(path) {
+  const stories = await getStories(path);
+  const hasStories = stories.length > 0;
+  view.innerHTML = `<div>${
+    hasStories ? stories.map((story) => JSON.stringify(story)) : 'No stories'
+  }</div>`;
 }
