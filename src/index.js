075ada070b7270648f503d265ffef1334a9febdc
Ronald Arias <ariasron@hotmail.com>
2021-05-17 10:52:38 -0500

    Changes color when an option menu is active

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 import RouterHandler from './router';
 
+function setActiveLink() {
+  const links = document.querySelectorAll('.header-link');
+  links.forEach((link) => {
+    const linkPath = link.getAttribute('href');
+    const currentPath = window.location.hash;
+    if (currentPath === linkPath) {
+      link.classList.add('active');
+    } else {
+      link.classList.remove('active');
+    }
+  });
+}
+
+window.onhashchange = () => {
+  setActiveLink();
+};
+
 class App {
   constructor() {
     new RouterHandler();
